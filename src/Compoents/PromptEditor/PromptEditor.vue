445a4e2e1cdce3ea6a5984416558c15f41e5f03e
ilank <ilank@163.com>
2023-06-07 18:18:23 +0800

    Update PromptEditor.vue

--- src/Compoents/PromptEditor/PromptEditor.vue
+++ src/Compoents/PromptEditor/PromptEditor.vue
@@ -36,8 +36,8 @@
                 </div>
                 <select v-model="promptEditor.data.server">
                     <option :value="LocalTrasnslateServer" :title="LocalTrasnslateServer">本地翻译接口</option>
-                    <option value="https://indexfs.moonvy.com:19213/prompt-studio">腾讯翻译</option>
-                    <option value="https://indexfs.moonvy.com:19213/prompt-studio2">腾讯翻译 2</option>
+                    <option value="https://indexfs.moonvy.com:19213/prompt-studio" disabled>腾讯翻译</option>
+                    <option value="https://indexfs.moonvy.com:19213/prompt-studio2" disabled>腾讯翻译 2</option>
                     <option value="https://indexfs.moonvy.com:19213/prompt-studio/ai" disabled>
                         OpenAI GPT-3.5 (WIP)
                     </option>
